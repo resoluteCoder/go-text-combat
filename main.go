129eee7798aa0efaa4553a27b018c2c4907edbf7
resoluteCoder <resolutecoder@gmail.com>
2023-07-13 13:00:48 -0500

    added more stuff

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,34 +25,34 @@ type Enemy struct {
 }
 
 type Combatant interface {
-	TakeDamage(int)
-	DisplayStatus()
+	takeDamage(int)
+	displayStatus()
 }
 
 type Living struct {
-	EntityId       int
-	Name           string
-	Health         int
-	PhysicalAttack int
-	PhysicalArmor  int
+	entityId       int
+	name           string
+	health         int
+	physicalAttack int
+	physicalArmor  int
 }
 
 func (living *Living) TakeDamage(damage int) {
-	totalDamage := living.PhysicalArmor - damage
-	living.Health -= int(math.Abs(float64(totalDamage)))
+	totalDamage := living.physicalArmor - damage
+	living.health -= int(math.Abs(float64(totalDamage)))
 }
 
 func (living *Living) DisplayStatus() {
-	fmt.Printf("%s is at %d health.\n", living.Name, living.Health)
+	fmt.Printf("%s is at %d health.\n", living.name, living.health)
 }
 
 func NewPlayer(name string) *Player {
 	playerStats := Living{
-		EntityId:       0,
-		Name:           name,
-		Health:         100,
-		PhysicalAttack: 10,
-		PhysicalArmor:  5,
+		entityId:       0,
+		name:           name,
+		health:         100,
+		physicalAttack: 10,
+		physicalArmor:  5,
 	}
 	player := Player{
 		playerStats,
@@ -61,11 +62,11 @@ func NewPlayer(name string) *Player {
 
 func NewEnemy(name string) *Enemy {
 	enemyStats := Living{
-		EntityId:       1,
-		Name:           name,
-		Health:         100,
-		PhysicalAttack: 5,
-		PhysicalArmor:  1,
+		entityId:       1,
+		name:           name,
+		health:         100,
+		physicalAttack: 5,
+		physicalArmor:  1,
 	}
 	enemy := Enemy{
 		enemyStats,
@@ -74,24 +75,51 @@ func NewEnemy(name string) *Enemy {
 }
 
 type CombatManager struct {
-	Combatants       []Combatant
+	enemies          []*Enemy
+	player           Player
 	isCombatFinished bool
 	isPlayerTurn     bool
 	currentIndex     int
 }
 
-func (combatManager CombatManager) PrintPlayerMenu() {
+func (combatManager CombatManager) printPlayerMenu() {
+	playerMenu := []string{"Your turn.", "1) Attack"}
+	for _, text := range playerMenu {
+		fmt.Println(text)
+	}
 }
 
-func (combatManager CombatManager) GetInputFromPlayer(inputText string) {
-	fmt.Println(inputText)
-	// STOPPED HERE
-	// reader := bufio.NewReader(os.Stdin)
-	// input, err := reader.ReadString('\n')
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+func (combatManager CombatManager) printAttackMenu() {
+	fmt.Println("Which target?")
+	for index, enemy := range combatManager.enemies {
+		fmt.Printf("%d) %s\n", index+1, enemy.name)
+	}
+}
 
+func (combatManager CombatManager) getInputFromPlayer() (string, error) {
+	fmt.Print("> ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	return strings.TrimSuffix(input, "\n"), nil
+}
+
+func (combatManager *CombatManager) handlePlayerAttack(targetIndex string) {
+	index, _ := strconv.ParseInt(targetIndex, 10, 16)
+	enemy := combatManager.enemies[index-1]
+	enemy.TakeDamage(combatManager.player.physicalAttack)
+	enemy.DisplayStatus()
+	combatManager.isPlayerTurn = false
+}
+
+func (combatManager *CombatManager) handleEnemyAttack() {
+	for _, enemy := range combatManager.enemies {
+		fmt.Println(enemy.name)
+	}
+	combatManager.isPlayerTurn = true
 }
 
 func main() {
@@ -99,53 +127,26 @@ func main() {
 	giant := NewEnemy("Giant")
 	skeleton := NewEnemy("Skeleton")
 
-	combatants := []Combatant{player, giant, skeleton}
+	enemies := []*Enemy{giant, skeleton}
 
 	combatManager := CombatManager{
-		Combatants:       combatants,
-		isCombatFinished: false,
-		isPlayerTurn:     true,
-		currentIndex:     0,
+		enemies:      enemies,
+		player:       *player,
+		isPlayerTurn: true,
+		currentIndex: 0,
 	}
 
-	for !combatManager.isCombatFinished {
+	for len(combatManager.enemies) != 0 {
 		if combatManager.isPlayerTurn {
-			combatManager.PrintPlayerMenu()
+			combatManager.printPlayerMenu()
+			input, _ := combatManager.getInputFromPlayer()
+			if input == "1" {
+				combatManager.printAttackMenu()
+				input, _ := combatManager.getInputFromPlayer()
+				combatManager.handlePlayerAttack(input)
+			}
+		} else {
+			combatManager.handleEnemyAttack()
 		}
 	}
-
-	for _, combatant := range combatants {
-		switch combatant.(type) {
-		case *Player:
-			combatant.DisplayStatus()
-			fmt.Println("Your turn")
-			fmt.Println("1) Attack")
-			fmt.Print("> ")
-			reader := bufio.NewReader(os.Stdin)
-			input, err := reader.ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			switch strings.TrimSuffix(input, "\n") {
-			case "1":
-				fmt.Println("Which target?")
-				for index, combatant := range combatants {
-					switch combatant.(type) {
-					case *Enemy:
-						fmt.Printf("%d) %s\n", index, combatant.(*Enemy).Name)
-					}
-				}
-				fmt.Print("> ")
-				input, err := reader.ReadString('\n')
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Print(input)
-			}
-		case *Enemy:
-			fmt.Printf("%s took their turn\n", combatant.(*Enemy).Name)
-		}
-	}
-
 }
